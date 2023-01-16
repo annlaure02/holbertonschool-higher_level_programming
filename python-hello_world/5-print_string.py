26e9bb2c07d902a8c981b9da0b571f3817428571
annlaure02 <alaure.g02@gmail.com>
2023-01-16 03:56:55 -0800

    added task 5 print string file

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str, end="")
-print(str, end="")
-print(str)
+print(str, str, str, sep="")
 print(str[0:9])
