6154526603d314de5881567ec52cfbc6bfb188a1
annlaure02 <alaure.g02@gmail.com>
2023-01-18 11:27:14 -0800

    Task 2 fixed pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
         print("{} arguments:".format(len(sys.argv) - 1))
 
     for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i])) 
+        print("{}: {}".format(i, sys.argv[i]))
