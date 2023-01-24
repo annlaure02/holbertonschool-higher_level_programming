8b0b5d27ee8ba926b0b62655e5a80edb5f10fcb0
annlaure02 <alaure.g02@gmail.com>
2023-01-24 07:41:32 -0800

    Task 1-replace all occurrences of an element by another in a new list

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i, element in enumerate(my_list):
+        if element == search:
+            new_list[i] = replace
+        else:
+            new_list[i] == my_list[i]
+    return new_list
