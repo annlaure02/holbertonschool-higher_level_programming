5ba5481a35b6691f85230ccbb37e0022cc83286c
annlaure02 <alaure.g02@gmail.com>
2023-01-26 09:56:49 -0800

    Task 0- print x elements of a list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        elements = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            elements += 1
+        print()
+        return elements
+    except:
+        return elements
