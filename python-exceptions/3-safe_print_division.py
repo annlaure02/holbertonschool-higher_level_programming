1507c6f3630a60cd73a5c24bdcb47f613be59719
annlaure02 <alaure.g02@gmail.com>
2023-01-26 12:44:41 -0800

    Task 3- correct excep

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,8 @@ def safe_print_division(a, b):
     try:
         result = a / b
         return result
-    except:
-        return
+    except ZeroDivisionError:
+        result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
