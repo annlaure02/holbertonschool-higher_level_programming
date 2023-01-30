622a168eaba535430046fd8ab37e3d43baa0cffd
annlaure02 <alaure.g02@gmail.com>
2023-01-30 07:47:46 -0800

    Task 4- cleaning my code

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,13 +6,6 @@ class Square:
     """ Square """
     def __init__(self, size=0):
         self.__size = size
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size ** 2
 
     @property
     def size(self):
@@ -25,3 +18,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        return self.__size ** 2
