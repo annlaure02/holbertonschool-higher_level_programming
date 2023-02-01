2c692a2890a3c75c2abdf01f8b4c0bf9607dfd77
annlaure02 <alaure.g02@gmail.com>
2023-02-01 02:42:02 -0800

    Task 1-added conditions on loop for

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,12 +10,20 @@ def matrix_divided(matrix, div):
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
-    for row in matrix:
-        for i in row:
-            if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+    if type(matrix) is not list or type(matrix[0]) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of \
+                integers/floats")
+    if (len(matrix) == 0) or (len(matrix[0]) == 0):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+                integers/floats")
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+                    integers/floats")
+        for i in row:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+                        integers/floats")
     return [[round(i/div, 2) for i in row] for row in matrix]
