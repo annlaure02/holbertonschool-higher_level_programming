5b82692947660639b7b05d4ef2555b2207146fc0
annlaure02 <alaure.g02@gmail.com>
2023-02-01 04:05:45 -0800

    Task 1- fixed pep8

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,12 +18,12 @@ integers/floats")
     for row in matrix:
         for i in row:
             if type(i) is not int and type(i) is not float:
-               raise TypeError("matrix must be a matrix (list of lists) of \
+                raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
     for row in matrix:
-         if len(row) != len(matrix[0]):
+        if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-            
+
     if div == float('inf'):
-            raise TypeError("div must be a number")
+        raise TypeError("div must be a number")
     return [[round(i/div, 2) for i in row] for row in matrix]
