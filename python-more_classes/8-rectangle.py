bbb56b748189f227a744c48e2700e2e2109747cc
annlaure02 <alaure.g02@gmail.com>
2023-02-02 11:36:03 -0800

    Task 8-fixed pep8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -71,5 +71,5 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() < rect_2.area():
-            return rect_2        
+            return rect_2
         return rect_1
