415175671126d54a617a13d25cf9d024d26d6f05
annlaure02 <alaure.g02@gmail.com>
2023-02-07 03:07:57 -0800

    Task 4- changed return true and false order

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ def inherits_from(obj, a_class):
     """ function that returns True if the object is an instance of,
 or if the object is an instance of a class that inherited from,
 the specified class ; otherwise False """
-    if type(obj) is a_class and isinstance(obj, a_class):
-        return False
-    else:
+    if type(obj) is not a_class and isinstance(obj, a_class):
         return True
+    else:
+        return False
