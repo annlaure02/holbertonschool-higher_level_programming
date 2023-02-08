f78291f21216b18738692a5002a0bd55d90d7280
annlaure02 <alaure.g02@gmail.com>
2023-02-08 08:15:17 -0800

    Task 7-correct code

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-filename = "add_item.json"
-args = sys.argv
-with open(filename, "a+", encoding="utf-8") as f:
-    my_list = []
-    my_list.extend(args[1:])
-    save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
+if __name__ == '__main__':
+    filename = "add_item.json"
+    args = sys.argv
+    with open(filename, "a", encoding="utf-8") as f:
+        try:
+            my_list = load_from_json_file(filename)
+        except:
+            my_list = []
+        my_list.extend(args[1:])
+        save_to_json_file(my_list, filename)
