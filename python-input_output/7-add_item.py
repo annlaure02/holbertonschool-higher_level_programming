1a6f6d270872173c77c05a13fde1cc87dedea823
annlaure02 <alaure.g02@gmail.com>
2023-02-08 07:30:32 -0800

    Task 7- adds all arguments to a Python list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 args = sys.argv
 with open(filename, "a+", encoding="utf-8") as f:
-    my_list = args[1:]
+    my_list = []
     my_list.extend(args[1:])
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
