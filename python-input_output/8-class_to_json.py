e8977aacccb78d1957d33c7d5eb574c048c93bba
annlaure02 <alaure.g02@gmail.com>
2023-02-08 12:35:56 -0800

    Task 8- fixed pep8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,4 +5,4 @@
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure
 for JSON serialization of an object """
-    return obj.__dict__ 
+    return obj.__dict__
