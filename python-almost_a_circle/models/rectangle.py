070acdb92c41a9c1536c8ffc9db33a51344d9fd4
annlaure02 <alaure.g02@gmail.com>
2023-02-13 04:10:37 -0800

    Task 2- fixed y in setter and correct all pep8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,11 +12,11 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-    
+
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
@@ -25,24 +25,24 @@ class Rectangle(Base):
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
- 	
+
     @height.setter
     def height(self, value):
-        if type(value)is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if type(value) is not int:
@@ -51,12 +51,12 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
-        
+
     @property
     def y(self):
         return self.__y
-    
-    @x.setter
+
+    @y.setter
     def y(self, value):
         if type(value) is not int:
             raise TypeError("y must be an integer")
