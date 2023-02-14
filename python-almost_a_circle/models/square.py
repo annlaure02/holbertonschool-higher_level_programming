86bc6f731e8439defd9835ca0b4c0b56a3741104
annlaure02 <alaure.g02@gmail.com>
2023-02-14 02:24:31 -0800

    Task 10- correct str

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,4 +10,4 @@ class Square(Rectangle):
         self.size = size
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x} {self.y} - {self.width }"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width }"
