25ef09a327e547c9f7f7625c165a3d9a66c94eb0
annlaure02 <alaure.g02@gmail.com>
2023-02-14 08:25:03 -0800

    Task 17- returns the list of the JSON string representation json_string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -34,3 +34,11 @@ class Base:
                 dict_list = [obj.to_dictionary() for obj in list_objs]
                 json_string = cls.to_json_string(dict_list)
                 f.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ return the list of the JSON string representation json_string """
+        if json_string is None or json_string == "":
+            return "[]"
+        else:
+            return json.loads(json_string)
