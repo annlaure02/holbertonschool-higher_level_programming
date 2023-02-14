e54434b1bf764ba13a4defe212f0342b0159b395
annlaure02 <alaure.g02@gmail.com>
2023-02-14 07:12:31 -0800

    Task 15- returns the JSON string representation of list_dictionaries

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ creation of the class Base """
+import json
 
 
 class Base:
@@ -7,8 +8,18 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ initialisation of id """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries """
+        if list_dictionaries is None or list_dictionaries == "":
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+            
