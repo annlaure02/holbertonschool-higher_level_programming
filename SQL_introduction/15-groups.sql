496c55f662b4ec6346fc46df349ee71e2ef2fed0
annlaure02 <alaure.g02@gmail.com>
2023-03-13 22:07:46 +0100

    Task 15-changed number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in the table second_table
 SELECT score,
-COUNT(*) AS number
+COUNT(*) 'number'
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
