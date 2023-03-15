dd67eb5aaf4199c634110ebac766d1374ea9dce6
annlaure02 <alaure.g02@gmail.com>
2023-03-15 11:10:28 +0100

    Task 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, display NULL
+-- Use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
