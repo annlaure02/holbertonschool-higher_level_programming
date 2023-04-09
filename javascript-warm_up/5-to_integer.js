b757d0b62d30bd14c9351518f86f44590fd1c5a8
annlaure02 <alaure.g02@gmail.com>
2023-04-09 22:29:43 +0200

    changed the message in the else

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2])
+const arg = parseInt(process.argv[2]);
 
 if (arg) {
   console.log('My number: ' + arg);
 } else {
-  console.log('No argument');
+  console.log('Not a number');
 }
