62ace40456621e14dc30ba0e63977b359c123915
annlaure02 <alaure.g02@gmail.com>
2023-04-10 00:12:07 +0200

    added file for task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (x) {
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
