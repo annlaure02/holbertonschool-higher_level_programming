c5df1f0f864ee88a7bf77eac6974328884678bd6
annlaure02 <alaure.g02@gmail.com>
2023-04-17 14:48:04 +0200

    remove space after :

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 
 request.get(process.argv[2], function (error, response) {
   console.error(error);
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
