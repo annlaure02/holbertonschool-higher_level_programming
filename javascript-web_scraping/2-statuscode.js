b5f0e46787f94f8697bd283078d41248d95ad85f
annlaure02 <alaure.g02@gmail.com>
2023-04-17 13:35:42 +0200

    added file for task 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], function (error, response) {
+  console.error(error);
+  console.log('code: ', response.statusCode);
+});
