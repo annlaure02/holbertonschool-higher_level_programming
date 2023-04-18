f5af5a040f05adafb9c7fdf3ec68bde30c20ae53
annlaure02 <alaure.g02@gmail.com>
2023-04-18 13:57:13 +0200

    added file for task 2

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 $(function () {
-  $('#red_header').on('click', function (event) {
-    $(event.delegateTarget).css('color', '#FF0000');
+  $('DIV#red_header').click (function () {
+    $('header').css('color', '#FF0000');
   });
 });
