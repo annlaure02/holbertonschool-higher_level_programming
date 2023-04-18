70a39210bf9a6d3825f3acbc6d1c4e1bd8b98e94
annlaure02 <alaure.g02@gmail.com>
2023-04-18 18:34:06 +0200

    added file for task 9

---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+$(function () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+  $.get(url, function (data) {
+    $('DIV#hello').append(data.hello);
+  });
+});
